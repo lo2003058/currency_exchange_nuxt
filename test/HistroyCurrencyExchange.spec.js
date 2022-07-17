32c943757dd19395c73df5d14202737d33287447
yin.lo <lo2003058@gmail.com>
2022-07-17 22:30:49 +0800

    try to use better method setup the dotenv file

---
+++ test/HistroyCurrencyExchange.spec.js
@@ -0,0 +1,24 @@
+// import axios from "axios";
+// import historyExchangeRateComponent from "@/components/historyExchangeRateComponent";
+//
+// jest.mock('axios');
+//
+// describe('fetchCurrency', () => {
+//   it('fetches successfully data from an API', async () => {
+//     const data = {
+//       data: {
+//
+//       },
+//     };
+//
+//     axios.get.mockImplementationOnce(() => Promise.resolve(data));
+//   });
+//
+//   it('fetches erroneously data from an API', async () => {
+//     const errorMessage = 'Network Error';
+//
+//     axios.get.mockImplementationOnce(() =>
+//       Promise.reject(new Error(errorMessage)),
+//     );
+//   });
+// });
